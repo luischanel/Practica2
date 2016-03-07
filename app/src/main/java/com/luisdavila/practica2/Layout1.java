6daf425fcccac06dd43377b2673431804e570464
luis <luischanel16@gmail.com>
2016-03-07 17:22:16 -0500

    item1

---
+++ app/src/main/java/com/luisdavila/practica2/Layout1.java
@@ -0,0 +1,13 @@
+package com.luisdavila.practica2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Layout1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout1);
+    }
+}
